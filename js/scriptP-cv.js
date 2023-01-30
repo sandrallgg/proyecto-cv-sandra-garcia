77739b18d99ea7ae347a8d762828337cad66dbc5
sandra garcia <sandrallgg@yahoo.com.ar>
2023-01-30 14:01:30 -0300

    Version para entrega del trabajo: curriculum Sandra Garcia

--- js/scriptP-cv.js
+++ js/scriptP-cv.js
@@ -1,46 +1,17 @@
-/**document.getElementById("ocultar-datos-personales").addEventListener('click',function(){
-    document.getElementById('datos-personales').style.display='none'
-    intercambiar_boton(true,'ocultar-datos-personales','mostrar-datos-personales');    
-})
-document.getElementById("mostrar-datos-personales").addEventListener('click',function(){
-    document.getElementById('datos-personales').style.display='block'
-    intercambiar_boton(false,'ocultar-datos-personales','mostrar-datos-personales');    
-})
-
-
-document.getElementById("menos-residencia").addEventListener('click',function(){
-    document.getElementById('datos-residencia').style.display='none'
-    intercambiar_boton(true,'menos-residencia','mas-residencia');   
-})
-document.getElementById("mas-residencia").addEventListener('click',function(){
-    document.getElementById('datos-residencia').style.display='block'
-    intercambiar_boton(false,'menos-residencia','mas-residencia');    
-})
-
-
-document.getElementById("menos-formacion").addEventListener('click',function(){
-    document.getElementById('datos-formacion').style.display='none'
-    intercambiar_boton(true,'menos-formacion','mas-formacion'); 
-})
-document.getElementById("mas-formacion").addEventListener('click',function(){
-    document.getElementById('datos-formacion').style.display='block'
-    intercambiar_boton(false,'menos-formacion','mas-formacion'); 
-}) **/
-
-
-
-
+/** Toma la accion del click para los botones mostrar y ocultar de cada seccion**/
 function tomar_click(menos,mas,seccion){
     document.getElementById(menos).addEventListener('click',function(){
+        /** cuando se hace click en ocultar, se esconde la seccion que tiene la informacion */
         document.getElementById(seccion).style.display='none'
         intercambiar_boton(true,menos,mas); 
     })
     document.getElementById(mas).addEventListener('click',function(){
+        /** cuando se hace click en mostrar, se pone visible la seccion que tiene la informacion */
         document.getElementById(seccion).style.display='block'
         intercambiar_boton(false,menos,mas);
     })
 }
-
+/** Intercambia que boton està visible, entre los botones mostrar y ocultar de cada seccion **/
 function intercambiar_boton(estado_boton1,boton1,boton2){
     if (estado_boton1) {
         document.getElementById(boton1).style.display='none';
@@ -48,9 +19,7 @@ function intercambiar_boton(estado_boton1,boton1,boton2){
     } else {
         document.getElementById(boton2).style.display='none';
         document.getElementById(boton1).style.display='block'  
-    }
-    
-    
+    }    
 }
 
 tomar_click("ocultar-datos-personales","mostrar-datos-personales",'datos-personales');
